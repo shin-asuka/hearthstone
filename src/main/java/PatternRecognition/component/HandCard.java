a5d7ad4840b52ca33d2bd1208ac1b11b7de0c391
yang.chao_1632 <yang.chao_1632@immomo.com>
2017-10-11 18:57:58 +0800

    commit

--- src/main/java/PatternRecognition/component/HandCard.java
+++ src/main/java/PatternRecognition/component/HandCard.java
@@ -13,32 +13,21 @@ public class HandCard {
     //TODO:pick point
 
     public static List<Point2D> p0 = Lists.newArrayList();
-
     public static List<Point2D> p1 = Lists.newArrayList();
-
     public static List<Point2D> p2 = Lists.newArrayList();
-
     public static List<Point2D> p3 = Lists.newArrayList();
-
     public static List<Point2D> p4 = Lists.newArrayList();
-
     public static List<Point2D> p5 = Lists.newArrayList();
-
     public static List<Point2D> p6 = Lists.newArrayList();
-
     public static List<Point2D> p7 = Lists.newArrayList();
-
     public static List<Point2D> p8 = Lists.newArrayList();
-
     public static List<Point2D> p9 = Lists.newArrayList();
-
     public static List<Point2D> p10 = Lists.newArrayList();
 
 
     //TODO:pick point
 
     public static List<List<Point2D>> pA0 = Lists.newArrayList();
-
     public static List<List<Point2D>> pA1 = Lists.newArrayList();
     public static List<List<Point2D>> pA2 = Lists.newArrayList();
     public static List<List<Point2D>> pA3 = Lists.newArrayList();
